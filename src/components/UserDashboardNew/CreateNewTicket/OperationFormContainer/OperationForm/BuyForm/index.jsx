7cea6f2df06fbff130ae98cb355a195895387078
marki <marcos.sk8.parengo@gmail.com>
2021-10-07 12:00:03 -0300

    fixes

--- src/components/UserDashboardNew/CreateNewTicket/OperationFormContainer/OperationForm/BuyForm/index.jsx
+++ src/components/UserDashboardNew/CreateNewTicket/OperationFormContainer/OperationForm/BuyForm/index.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React,{createRef} from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Row } from 'react-bootstrap'
 import FoundCard from './FoundCard';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronLeft,faChevronRight } from '@fortawesome/free-solid-svg-icons'
 const BuyForm = ({  data, setData, some, setSome}) => {
 
     const founds = [{
@@ -77,17 +78,88 @@ const BuyForm = ({  data, setData, some, setSome}) => {
             value: 75,
         }]
 
-    },]
+    },{
+        name: "CryptoCurrency found",
+        totalFeeParts: 1000,
+        feePartsAvalilable: 37,
+        feePartsValue: 30,
+        composition: [{
+            label: 'Bitcoin USD',
+            value: 15
+        }, {
+            label: 'Ethereum USD',
+            value: 5
+        }, {
+            label: 'HEX USD',
+            value: 7
+        }, {
+            label: 'Tether USD',
+            value: 0.2
+        }, {
+            label: 'Cardano USD',
+            value: 5
+        }, {
+            label: 'BinanceCoin US',
+            value: 15
+        }]
+
+    },  {
+        name: "CryptoCurrency found",
+        totalFeeParts: 10000,
+        feePartsAvalilable: 1000,
+        feePartsValue: 6,
+        composition: [{
+            label: 'Bitcoin USD',
+            value: 25
+        },{
+            label: 'Cardano USD',
+            value: 5
+        },{
+            label: 'Ethereum USD',
+            value: 75,
+        }]
+
+    },{
+        name: "CryptoCurrency found",
+        totalFeeParts: 10000,
+        feePartsAvalilable: 1000,
+        feePartsValue: 6,
+        composition: [{
+            label: 'Bitcoin USD',
+            value: 25
+        },{
+            label: 'Cardano USD',
+            value: 5
+        },{
+            label: 'Ethereum USD',
+            value: 75,
+        }]
+    }]
+    const foundsContainer= createRef()
+
+    const scrollFoundContainer=(right)=>{
+        if(right){
+            foundsContainer.current.scrollLeft += 200
+        }else{
+            foundsContainer.current.scrollLeft -= 200
+        }
+    }
 
     return (
-        <div>
-            <Row className="flex-row flex-nowrap overflow-auto">
+        <div className="p-relative">
+            <Row className="flex-row flex-nowrap overflow-auto" ref={foundsContainer}>
                 {founds.map((found, key) => {
                     return (
                         <FoundCard key={key} ownKey={key} found={found} data={data} setData={setData} some={some} setSome={setSome}/>
                     )
                 })}
             </Row>
+            <div className="arrow right" onClick={()=>scrollFoundContainer(true)}>
+                <FontAwesomeIcon icon={faChevronRight}/>
+            </div>
+            <div className="arrow left" onClick={()=>scrollFoundContainer(false)}>
+                <FontAwesomeIcon icon={faChevronLeft}/>
+            </div>
         </div>
     )
 }
