d91385d4870445027e807450f140339cdb033da5
MarcosParengo <marcos.sk8.parengo@gmail.com>
2021-10-28 23:29:54 -0300

    Cambios generales antes de arrancar con la api de fondos

--- src/components/UserDashboardNew/User/MovementsTable/CardsContainer/MainCard/FoundDetail/index.jsx
+++ src/components/UserDashboardNew/User/MovementsTable/CardsContainer/MainCard/FoundDetail/index.jsx
@@ -3,15 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { useTranslation } from "react-i18next";
 
-
-import moment from 'moment';
-
-
 const FoundDetail = () => {
-
-    const { t } = useTranslation();
-    history.push(`/login`);
-
     return (
         <div>
             <h1>Grafico</h1>
