d7cf6a4ea6332d7c03e29cec3d920f3e79ad7b79
marcos gaston parengo <marcos.sk8.parengo@gmail.com>
2021-11-18 20:13:58 -0300

    ReCharts

---
+++ src/components/UserDashboardNew/User/MovementsTable/CardsContainer/MainCard/MainCardFund/FundDetail/Chart/index.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ReferenceLine,ResponsiveContainer,Brush } from 'recharts';
+import './index.css'
+import moment from 'moment';
+
+const Chart = ({ NavInfoToggled, Height, Width }) => {
+
+
+
+    const generateData = (amount = 10, max = 100, min = 0) => {
+        let data = []
+
+        for (let index = amount; index > 0; index--) {
+            let todayMoment = moment()
+            const date = todayMoment.subtract(index, "days").format('MMM DD YY')
+            const price = (Math.random() * (max - min) + min).toFixed(2);;
+
+            data.push({
+                date: date,
+                price: price
+            })
+        }
+        return data
+    }
+    let data=generateData(20,100,30)
+
+    return (
+        <ResponsiveContainer width="100%" height={Height}>
+            <LineChart width={Width} height={Height} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+                <Line type="monotone" dataKey="uv" stroke="#808080" />
+                <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                <XAxis dataKey="date" angle={0} dx={20} />        
+                <YAxis />
+                <Tooltip />
+                <ReferenceLine y={80} label="Max" stroke="red" />
+                <Line type="monotone" dataKey="price" stroke="#808080" />
+                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+                <Brush />
+            </LineChart>
+        </ResponsiveContainer> 
+    )
+}
+export default Chart
+
--- src/components/UserDashboardNew/User/MovementsTable/CardsContainer/MainCard/MainCardFund/FundDetail/index.jsx
+++ src/components/UserDashboardNew/User/MovementsTable/CardsContainer/MainCard/MainCardFund/FundDetail/index.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { createRef,useEffect,useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Chart from './Chart';
 
-const FundDetail = ({NavInfoToggled}) => {
+const FundDetail = ({ NavInfoToggled }) => {
+    const chartContainer = createRef()
+    const [Width,setWidth]=useState(0)
+    const [Height,setHeight]=useState(0)
+
+    useEffect(() => {
+        const isNull = () => {
+            return (chartContainer.current === null)
+        }
+        if(!isNull()){
+            setWidth(chartContainer.current.clientWidth)
+            setHeight(chartContainer.current.clientHeight)
+        }
+    }, [chartContainer])
+    
     return (
-        <div className={`mb-2 ${NavInfoToggled? "movementsTable-navInfoToggled" : "movementsTable" }`}>
-
+        <div  ref={chartContainer} className={`mb-2 ${NavInfoToggled ? "movementsTable-navInfoToggled" : "movementsTable"} `}>
+            <Chart Height={Height} Width={Width}/>
         </div>
     )
 }
