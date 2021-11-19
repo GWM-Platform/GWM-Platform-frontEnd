161b14e0d9242cfe8440bf229ce0c9b0195bc9df
marcos gaston parengo <marcos.sk8.parengo@gmail.com>
2021-11-19 15:14:47 -0300

    fix

---
+++ src/components/UserDashboardNew/User/newTicket/NoFunds/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useTranslation } from "react-i18next";
+
+const NoFunds = () => {
+    const { t } = useTranslation();
+
+    return (
+        <div class="h-100 d-flex align-items-center justify-content-center">
+            <h1>
+                {t("There are no funds available for the operation")}
+            </h1>
+        </div>
+    )
+}
+export default NoFunds
