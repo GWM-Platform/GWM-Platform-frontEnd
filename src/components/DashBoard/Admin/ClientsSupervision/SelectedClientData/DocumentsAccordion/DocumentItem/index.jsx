ddfd584096a75601caed30e82e98fa7e3ffb3530
Marcos Parengo <marcos.sk8.parengo@gmail.com>
2023-01-26 14:27:25 -0300

    feat(Documents): visual adjustments

--- src/components/DashBoard/Admin/ClientsSupervision/SelectedClientData/DocumentsAccordion/DocumentItem/index.jsx
+++ src/components/DashBoard/Admin/ClientsSupervision/SelectedClientData/DocumentsAccordion/DocumentItem/index.jsx
@@ -1,73 +1,135 @@
+import { faChevronDown, faChevronUp, faDownload } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import MoreButton from "components/DashBoard/GeneralUse/MoreButton";
-import React from "react";
+import React, { useState } from "react";
 import { Fragment } from "react";
-import { Badge, Dropdown, Spinner } from "react-bootstrap";
+import { Badge, Col, Dropdown, Spinner, Button } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 
 const DocumentItem = ({ document }) => {
     const { t } = useTranslation()
 
+    const [TagsCollapsed, setTagsCollapsed] = useState(true)
+    const expandTags = () => { setTagsCollapsed(false) }
+    const collapseTags = () => { setTagsCollapsed(true) }
+
+    const tagsAmount = document?.tags?.length
+    const hasTags = !!(document?.tags?.length > 0)
+    const hasMultipleTags = !!(document?.tags?.length > 1)
+
     return (
-        <div className="d-flex Actions py-2 align-items-center document" style={{ borderBottom: " 1px solid lightgray" }}>
-            <div className="mb-0 pe-1 pe-md-2" >
-                <h1 className="title d-flex align-items-center">{t("Document")}&nbsp;#{document.id}:&nbsp;
-                    <a target="_blank" rel="noreferrer nofollow" href={document.link}>{document.name}</a>
-                </h1>
-                {
-                    !!(document?.tags) &&
-                    <>
-                        <h2 className="tags">
-                            {t("Tags")}:
+        <Col xs="12" md="6" lg="4">
+            <div className="p-2 document" >
+
+                <div className="d-flex Actions justify-content-between mb-3">
+
+                    <div className="mb-0 pe-1 pe-md-2" >
+                        <div className="d-flex align-items-center">
+                            <a className="title d-inline" target="_blank" rel="noreferrer nofollow" href={document.link}>{document.name}</a>
+                        </div>
+                        <h2 className="identifier d-flex align-items-center">
+                            {t("Document")}&nbsp;#{document.id}
                         </h2>
+                    </div>
+
+                    <div className="ms-auto">
                         {
-                            document?.tags &&
-                            <>
-                                {document?.tags.map(tag =>
-                                    <Fragment key={`document-${document.id}-tag-${tag}`}>
-                                        <Badge bg="primary">
-                                            {tag}
-                                        </Badge>
-                                        &nbsp;
-                                    </Fragment>
-                                )}
-                            </>
+                            Request.fetching ?
+                                <Spinner
+                                    as="span"
+                                    animation="border"
+                                    size="sm"
+                                    role="status"
+                                    aria-hidden="true"
+                                />
+                                :
+                                <Dropdown
+                                    id={`dropdown-button-drop-start`}
+                                    variant="secondary"
+                                    title={t(`Document options`)}
+                                    className="d-flex justify-content-end"
+                                    disabled={Request.fetching}
+                                >
+                                    <Dropdown.Toggle as={MoreButton} id="dropdown-custom-components" />
+                                    <Dropdown.Menu >
+
+                                        <Dropdown.Item target="_blank" rel="noreferrer nofollow" href={document.link}>
+                                            {t('Open in a new tab')}
+                                        </Dropdown.Item>
+                                        <Dropdown.Item disabled>
+                                            {t('Download')}
+                                        </Dropdown.Item>
+                                        <Dropdown.Divider />
+                                        <Dropdown.Item disabled target="_blank" rel="noreferrer nofollow" href={document.link}>
+                                            {t('Edit')}
+                                        </Dropdown.Item>
+                                        <Dropdown.Item disabled>
+                                            {t('Delete')}
+                                        </Dropdown.Item>
+                                    </Dropdown.Menu >
+                                </Dropdown>
                         }
-                    </>
-                }
+                    </div>
+
+                </div>
+                <div>
+                    <div className={`d-flex w-100 overflow-hidden align-items-end`}>
+
+                        {!!(hasTags) &&
+                            <div className={`d-flex overflow-hidden ${TagsCollapsed ? "" : "flex-wrap"} me-1`}
+                                style={{
+                                    rowGap: "4px",
+                                    columnGap: "4px"
+                                }}
+                            >
+                                {
+                                    TagsCollapsed ?
+                                        <>
+                                            <Badge className="tag overview" bg="secondary">
+                                                {document?.tags[0]}
+                                            </Badge>
+                                            {
+                                                hasMultipleTags &&
+                                                <>
+                                                    <Badge className="tag" bg="secondary">
+                                                        +{tagsAmount - 1}
+                                                    </Badge>
+                                                    <Button
+                                                        onClick={() => expandTags()}
+                                                        type="button" as={Badge}
+                                                        className="tag text-nowrap" bg="primary"
+                                                        title={t("Show more tags")}>
+                                                        <FontAwesomeIcon icon={faChevronDown} />
+                                                    </Button>
+                                                </>
+                                            }
+                                        </>
+                                        :
+                                        <>
+                                            {document?.tags.map(tag =>
+                                                <Fragment key={`document-${document.id}-tag-${tag}`}>
+                                                    <Badge className="tag" bg="secondary">
+                                                        {tag}
+                                                    </Badge>
+                                                </Fragment>
+                                            )}
+
+                                            <Button as={Badge} bg="primary" title={t("Collapse tags")} type="button" className="noStyle d-inline-block" onClick={() => collapseTags()}>
+                                                <FontAwesomeIcon icon={faChevronUp} />
+                                            </Button>
+                                        </>
+                                }
+                            </div>
+                        }
+                        {
+                            TagsCollapsed &&
+                            <Button as={Badge} bg="primary" title={t("Download")} type="button" className="noStyle d-inline-block ms-auto"><FontAwesomeIcon icon={faDownload} /></Button>
+                        }
+                    </div>
+                </div>
             </div>
 
-            <div className="ms-auto">
-                {
-                    Request.fetching ?
-                        <Spinner
-                            as="span"
-                            animation="border"
-                            size="sm"
-                            role="status"
-                            aria-hidden="true"
-                        />
-                        :
-                        <Dropdown
-                            id={`dropdown-button-drop-start`}
-                            drop="start"
-                            variant="secondary"
-                            title={t(`Document options`)}
-                            className="d-flex justify-content-end"
-                            disabled={Request.fetching}
-                        >
-                            <Dropdown.Toggle as={MoreButton} id="dropdown-custom-components" />
-                            <Dropdown.Menu >
-                                <Dropdown.Item disabled>
-                                    {t('Delete')}
-                                </Dropdown.Item>
-                                <Dropdown.Item disabled>
-                                    {t('Edit')}
-                                </Dropdown.Item>
-                            </Dropdown.Menu >
-                        </Dropdown>
-                }
-            </div>
-        </div>
+        </Col>
     );
 }
 
--- src/components/DashBoard/Admin/ClientsSupervision/SelectedClientData/DocumentsAccordion/index.jsx
+++ src/components/DashBoard/Admin/ClientsSupervision/SelectedClientData/DocumentsAccordion/index.jsx
@@ -5,12 +5,14 @@ import { useContext } from "react";
 import { useState } from "react";
 import { useCallback } from "react";
 import { useEffect } from "react";
-import { Accordion, Button } from "react-bootstrap";
+import { Accordion, Button, Row } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 import './index.scss'
 import DocumentItem from "./DocumentItem";
 import ReactSelect from "react-select";
+import Loading from 'components/DashBoard/GeneralUse/Loading';
+import NoMovements from "components/DashBoard/GeneralUse/NoMovements";
 
 const DocumentsAccordion = ({ client }) => {
     const { t } = useTranslation()
@@ -60,7 +62,7 @@ const DocumentsAccordion = ({ client }) => {
         setSelectedOptions([...selectedOption]);
     };
 
-    const uniqueTagsOptions = () => [...new Set(documents.content.map(document => [document.tags]).flat(2))].map(tag => ({ value: tag, label: tag }))
+    const uniqueTagsOptions = () => [...new Set(documents.content.map(document => [document?.tags || []]).flat(2))].map(tag => ({ value: tag, label: tag }))
 
     const filteredDocuments = () => selectedOptions.length > 0
         ? documents.content.filter(
@@ -73,24 +75,40 @@ const DocumentsAccordion = ({ client }) => {
             <Accordion.Item eventKey="0">
                 <Accordion.Header>{t("Documentation")}</Accordion.Header>
                 <Accordion.Body className="documentsList">
-                    <ReactSelect
-                        value={selectedOptions}
-                        onChange={handleChange}
-                        className="w-100 mb-2"
-                        isMulti isClearable noOptionsMessage={() => t("No options")} placeholder={t("Filter documents by tags...")}
-                        options={[...uniqueTagsOptions()]}
-                        classNames={{
-                            multiValue: () => ("multiValue"),
-                            multiValueLabel: () => ("multiValueLabel"),
-                            multiValueRemove: () => ("multiValueRemove"),
-                        }}
-                    />
-                    {filteredDocuments().map(document => <DocumentItem document={document} key={`document-item-${client.id}-${document.id}`} />)}
-                    <div className="mt-2 d-flex justify-content-end">
-                        <Link to={`/DashBoard/clientsSupervision/${client.id}/addDocument`}>
-                            <Button>{t("Add document")}</Button>
-                        </Link>
-                    </div>
+                    {
+                        documents.fetching
+                            ?
+                            <Loading movements={4} />
+                            :
+                            <>
+                                <ReactSelect
+                                    value={selectedOptions}
+                                    onChange={handleChange}
+                                    className="w-100 mb-2"
+                                    isMulti isClearable noOptionsMessage={() => t("No options")} placeholder={t("Filter documents by tags...")}
+                                    options={[...uniqueTagsOptions()]}
+                                    classNames={{
+                                        multiValue: () => ("multiValue"),
+                                        multiValueLabel: () => ("multiValueLabel"),
+                                        multiValueRemove: () => ("multiValueRemove"),
+                                    }}
+                                />
+                                {
+                                    documents.fetched && filteredDocuments().length === 0 ?
+                                        <NoMovements movements={4} />
+                                        :
+                                        <Row className='g-2 mb-2'>
+                                            {filteredDocuments().map(document => <DocumentItem document={document} key={`document-item-${client.id}-${document.id}`} />)}
+                                        </Row>
+                                }
+                                <div className="mt-2 d-flex justify-content-end">
+                                    <Link to={`/DashBoard/clientsSupervision/${client.id}/addDocument`}>
+                                        <Button>{t("Add document")}</Button>
+                                    </Link>
+                                </div>
+                            </>
+                    }
+
                 </Accordion.Body>
             </Accordion.Item>
         </Accordion>
